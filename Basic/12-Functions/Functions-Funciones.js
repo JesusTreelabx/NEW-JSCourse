8b1f4dda7017bbbcf4ac9bb9acbafeb127e42aa2
Jesus Capetillo <jesscvdevops@gmail.com>
2025-08-08 12:53:06 -0600

    ADD: update new files pt13

--- Basic/12-Functions/Functions-Funciones.js
+++ Basic/12-Functions/Functions-Funciones.js
@@ -1,8 +1,10 @@
 
 
-// FGunctions (Funciones)
+// Functions (Funciones)
 
 // Estructura por defecto de una Funcion.
+
+// Simple
 function myFunc() {
     console.log("Hola, Mundo!")
 }
@@ -10,5 +12,92 @@ function myFunc() {
 myFunc()
 myFunc()
 myFunc()
-myFunc()
-myFunc()
+
+
+
+// for (let i = 0: i < 5; i++) {
+//     myFunc()
+// }
+
+
+
+
+// Con parametros
+function myFuncWithParams(name) {
+    console.log(` Hola, ${name}!`)
+}
+
+myFuncWithParams("Ana Katy")
+myFuncWithParams("Ronaldo")
+
+
+
+
+
+// Funciones Anonimas. Se tienen que asignar a una variable a una constante, si no no habria manera de poder invocarla.
+const myFunc2 = function(name) {
+    console.log(`Hola, ${name}!`)
+}
+
+myFunc2("Kike")
+
+
+
+
+
+
+// Arrow Functions. Tambien tenemos que terminar asignandolas a una variable o una constante.
+const myFunc3 = (name) => {
+    console.log(`Hola, ${name}!`)
+}
+
+myFunc3("Paola")
+
+
+
+const myFunc4 = (name) => console.log(`Hola, ${name}!`)
+
+myFunc4("Jessy")
+
+
+
+
+
+
+// Parametros
+function mySum(a, b) {
+    console.log(a + b)
+}
+
+mySum(5, 10)
+mySum(5)
+
+
+function defaultSum(a = 0, b = 0) {
+    console.log(a + b)
+}
+
+defaultSum()
+defaultSum(5)
+defaultSum(5, 10)
+defaultSum(b = 5)
+
+
+
+
+
+
+// Retorno de valores
+function multiplicar(a, b) {
+    return a * b
+}
+
+let result = multiplicar(5, 10)
+console.log(result)
+
+
+
+
+
+
+// Funciones anidadas
