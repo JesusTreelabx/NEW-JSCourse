e44f4c1c11afcb8d4eac2bdd7351d96246b8796f
Jesus Capetillo <jesscvdevops@gmail.com>
2025-08-13 15:53:42 -0600

    ADD: update new files pt16

--- Basic/12-Functions/Functions-Exercises/08-exercise.js
+++ Basic/12-Functions/Functions-Exercises/08-exercise.js
@@ -1,3 +1,16 @@
 
 
 // 8. Crea una funcion que reciba un array de numeros y devuelva un array con cada numero elevado al cuadrado.
+function arrayNum2(arr2) {
+    const newArray = []
+
+    for (let i = 0; i < arr2.length; i++) {
+            newArray.push(arr2[i] * arr2[i])
+    }
+    return newArray;
+}
+
+console.log(arrayNum2([5, 8, 4, 10, 30]))
+console.log(arrayNum2([25, 7, 9, 2, 3]))
+console.log(arrayNum2([1, 2, 3, 4, 5]))
+console.log(arrayNum2([11, 24, 6, 18, 12]))
---
+++ Basic/12-Functions/Functions-Exercises/09-exercise.js
@@ -0,0 +1,42 @@
+
+
+/* 9. Crea una funcion que reciba una cadena de texto y devuelva la misma cadena 
+con las palabras en orden inverso. */
+function stringInverso(param1, param2, param3, param4, param5) {
+    console.log(cadtext)
+}
+
+stringInverso("Its show time!")
+
+
+
+function nombreMayus(nombre) {
+    return nombre.toUpperCase()
+}
+
+console.log(nombreMayus("juan"))
+
+
+
+
+function saludoAnimal(animal) {
+    return `Hola ${animal}`
+}
+
+
+console.log(saludoAnimal("perro"))
+console.log(saludoAnimal("Gato"))
+console.log(saludoAnimal("Pato"))
+console.log(saludoAnimal("Tortuga"))
+console.log(saludoAnimal("Jirafa"))
+
+
+
+function ligamxFinalists(equipos) {
+    console.log(equipos)
+}
+
+ligamxFinalists(["Cruz Azul", "America", "Chivas", "Rayados", "Tigres", "Pumas"])
+ligamxFinalists(["Toluca", "Atlas", "QueretaroFC", "Puebla"])
+ligamxFinalists(["Xolos", "Necaxa", "Juarez", "Santos"])
+ligamxFinalists(["Mazatlan", "Atletico SL", "Leon", "Pachuca"])
