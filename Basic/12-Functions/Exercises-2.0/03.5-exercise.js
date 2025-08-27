86b0bca61e08fce603136cc0c73003e9c9c5645e
Jesus Capetillo <jesscvdevops@gmail.com>
2025-08-27 16:09:19 -0600

    ADD: new update files pt22

--- Basic/12-Functions/Exercises-2.0/03.5-exercise.js
+++ Basic/12-Functions/Exercises-2.0/03.5-exercise.js
@@ -7,13 +7,10 @@ mensaje con el precio total.
 Cada ingrediente tiene un costo adicional de $0.5, y el helado base cuesta $3. 
 La función debe retornar un string como: "Tu helado con [ingredientes] cuesta $[total]". */
 
+function pedidoHeladoDQ(ingredientes) {
+    let heladoBase = 3
+    let sum = heladoBase
 
-// Personalización de los helados.
-function personzalizarHeladoDQ(ingredientes) {
-    let precioBase = 3
-    let sum = precioBase
-
-    // Recorremos el arreglo de ingredientes.
     for(let i = 0; i < ingredientes.length; i++) {
         if(ingredientes[i] === "chocolate") {
             sum += 0.5
@@ -25,10 +22,10 @@ function personzalizarHeladoDQ(ingredientes) {
             sum += 0.5
         }
     }
-    
+
     return "Tu helado con " + ingredientes.join(" y ") + " cuesta $" + sum
 }
 
-
-console.log(personzalizarHeladoDQ(["chocolate", "caramelo"]))
-console.log(personzalizarHeladoDQ(["chocolate"]))
+console.log(pedidoHeladoDQ(["chocolate"]))
+console.log(pedidoHeladoDQ(["fresa", "caramelo"]))
+console.log(pedidoHeladoDQ(["fresas", "chocolate"]))
---
+++ Basic/12-Functions/Exercises-2.0/04-exercise.js
@@ -0,0 +1,11 @@
+
+
+// Tiempo de preparación en Starbucks
+/* Crea una funcion llamada tiempoPreparacionStarbucks que recibe el tipo de bebida
+("latte", "frapuccino", "te") yla cantidad de bebidas pedidas. Un latte toma 3 min.
+un frapuccino 5 min. y un te 2 min. por bebida.
+La funcion debe devolver el tiempo d total de preparacion en minutos. */
+
+function tiempoPreparacionStarbucks() {
+    
+}
