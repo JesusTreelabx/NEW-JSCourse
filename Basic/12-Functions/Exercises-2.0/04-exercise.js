d7a6aa3e29e7e5a9f00212196517e68cead25cbf
Jesus Capetillo <jesscvdevops@gmail.com>
2025-08-28 09:15:52 -0600

    ADD: update new files pt23

--- Basic/12-Functions/Exercises-2.0/04-exercise.js
+++ Basic/12-Functions/Exercises-2.0/04-exercise.js
@@ -2,10 +2,27 @@
 
 // Tiempo de preparación en Starbucks
 /* Crea una funcion llamada tiempoPreparacionStarbucks que recibe el tipo de bebida
-("latte", "frapuccino", "te") yla cantidad de bebidas pedidas. Un latte toma 3 min.
+("latte", "frapuccino", "te") y la cantidad de bebidas pedidas. Un latte toma 3 min.
 un frapuccino 5 min. y un te 2 min. por bebida.
 La funcion debe devolver el tiempo d total de preparacion en minutos. */
 
-function tiempoPreparacionStarbucks() {
-    
+function tiempoPreparacionStarbucks(bebida) {
+    let sum = 0
+
+    for(let i = 0; i < bebida.length; i++) {
+        if(bebida[i] === "latte") {
+            sum += 3 
+        }
+        if(bebida[i] === "frapuccino") {
+            sum += 5
+        }
+        if(bebida[i] === "te") {
+            sum += 2
+        }
+    }
+    return "Su bebida " + bebida + " estara lista en " + sum + " minutos"
 }
+
+
+console.log(tiempoPreparacionStarbucks("latte"))
+console.log(tiempoPreparacionStarbucks("frapuccino"))
